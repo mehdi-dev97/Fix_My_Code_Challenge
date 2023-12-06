9adbce8f9f3861b19aed2dd0a97f54ce46bb68e7
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-12-06 11:24:34 +0100

    Set documentation and rename methods.

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Square module class """
+
+class square():
+    """ Square class """
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        """ Initialize square class """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Print format representation """
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    """ New instance of square """
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
